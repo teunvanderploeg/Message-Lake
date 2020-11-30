ef6fa1f19a5ac0a28ca0c7596dbccf76fbb08a84
Teun van der Ploeg <29253@ma-web.nl>
2020-11-30 13:42:40 +0100

    tailwind
    tailwind proberen te instaleren en werkend te krijgen maar werkt nog niet helemaal

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    {{-- <link rel="stylesheet" href="../resources/css/app.css"> --}}
+    <link href="{{URL::asset('css/app.css')}}" rel="stylesheet" >
 </head>
 <body>
-    <h1>Welkom on massage lake</h1>
+    <h1 class="bg-white">Welkom on massage lake</h1>
     <p>
         Titel: {{ $post->title }}<br>
         Content: {{ $post->content }}<br>
