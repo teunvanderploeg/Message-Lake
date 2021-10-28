f48e0fbbd0661f97ba360499e99981396f034ff5
teunvanderploeg <29253@ma-web.nl>
2021-10-28 20:03:21 +0200

    Cleaned Homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,8 @@ use App\Models\MijnPost;
 
 class HomeController extends Controller
 {
-    //
     public function home()
     {
-
-        // $post = DB::table('mijn_posts')->get()->paginate(3);
-        $post = MijnPost::paginate(20);
-
-        return view('home', ['posts' => $post]);
+        return view('home', ['posts' => MijnPost::paginate(20)]);
     }
 }
