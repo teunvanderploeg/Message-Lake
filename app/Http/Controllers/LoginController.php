8979eae6f1df9a30ab77c8e10379536247e575d4
teunvanderploeg <29253@ma-web.nl>
2021-10-28 21:06:19 +0200

    Cleaned login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,17 +6,7 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //loginpage
     public function loginpage(){
         return view('login');
-    }
-
-    public function login(Request $request){
-        $data = $request->validate(
-            [
-                'email' => 'required|min:4|max:50',
-                'password' => 'required|min:4|max:50',
-            ]
-            );
     }
 }
